e5205180953a5d84776a667c7edb1b84f7ac9db5
John Leon <jleon253@gmail.com>
2026-04-22 16:50:17 -0500

    feat: Added enum files

---
+++ src/shared/enums/product.enum.ts
@@ -0,0 +1,44 @@
+export enum PriceType {
+  NORMAL = "NORMAL",
+  INTERNET = "INTERNET",
+  CMR = "CMR",
+}
+
+export enum UnitType {
+  UNIT = "UND",
+  OTHER = "OTHER",
+}
+
+export enum BadgeType {
+  DISCOUNT = "DISCOUNT",
+  BANK_PROMOTION = "BANK_PROMOTION",
+}
+
+export enum Highlights {
+  HEIGHT = "ALTO",
+  WIDTH = "ANCHO",
+  LENGTH = "LARGO",
+  MATERIAL = "MATERIAL",
+  MODEL = "MODELO",
+  TYPE = "TIPO",
+  USAGE = "USO",
+}
+
+export enum OnImageHover {
+  Zoom = "ZOOM",
+}
+
+export enum MediaType {
+  Sku = "SKU",
+}
+
+export enum Symbol {
+  COP = "$",
+}
+
+export enum SortOrder {
+  NAME_ASC = 'name_asc',
+  BRAND_ASC = 'brand_asc',
+  PRICE_ASC = 'price_asc',
+  PRICE_DESC = 'price_desc',
+}
